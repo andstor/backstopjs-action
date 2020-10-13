df037eb027da2768960676e28de4963b510148fd
André Storhaug <andr3.storhaug@gmail.com>
2020-10-13 22:06:29 +0200

    Test

--- src/installer.js
+++ src/installer.js
@@ -4,24 +4,24 @@ const path = require('path');
 
 async function installBackstopjs() {
   const actionDir = path.join(__dirname, '../');
-  
-  core.info(__dirname);
-  
+
   let cmd = 'npm';
   let args = ['install', 'backstopjs', '--production', '--parseable'];
   core.info('Installing BackstopJS...');
+  core.debug(`Command: ${cmd} ${args}`);
 
   let installOutput = '';
   const options = {};
   options.listeners = {
-      stdout: (data) => {
-          installOutput += data.toString();
-      }
+    stdout: (data) => {
+      installOutput += data.toString();
+    }
   };
   options.cwd = actionDir;
 
   await exec.exec(cmd, args, options);
   core.debug(`Installation output: ${installOutput}`);
+  return;
 }
 
 module.exports = {
